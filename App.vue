1441f76bdb9f7de80e3e7bb5f645eb275282bbe0
adityauparkar <64306202+adityauparkar@users.noreply.github.com>
2024-08-03 16:11:51 +1000

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,41 @@
+<template>
+ <h1>{{ title }}</h1>
+ <p>Welcome...</p> 
+ <div v-if="showModal">
+  <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+ </div>
+ <button @click.alt="toggleModal">open modal</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components:{Modal},
+  data(){
+    return{
+      title:'My first vue App :)',
+      header:'sign up for the giveaway',
+      text:'grab your ninja swag for half price',
+      showModal: false
+    }
+  },
+  methods:{
+    toggleModal(){
+      this.showModal=!this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
