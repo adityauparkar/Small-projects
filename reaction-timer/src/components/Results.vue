7ac94ea4144015eb0305bbf5635c1a5384de318f
adityauparkar <64306202+adityauparkar@users.noreply.github.com>
2024-08-22 15:30:32 +1000

    Update Results.vue

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,6 +1,8 @@
 <template>
+  <div class="result">
   <p>Reaction time - {{ score }} ms</p>
   <p class="rank">{{ rank }}</p>
+    </div>
 </template>
 
 <script>
@@ -13,17 +15,38 @@ export default {
   },
   mounted() {
     if (this.score < 250) {
-      this.rank = 'Ninja Fingers'
+      this.rank = 'Faster than light'
     } else if (this.score < 400) {
       this.rank = 'Rapid Reflexes'
     } else {
-      this.rank = '...Snail Pace'
+      this.rank = 'As slow as a Snail....'
     }
   }
 }
 </script>
 
 <style>
+  .result{
+    width: 190px;
+padding: 10px;
+background: #E8CF24;
+overflow:auto;
+display:block;
+margin: 40px auto;
+
+font-family: Verdana, Geneva, sans-serif; 
+font-size: 1.4rem;
+font-weight: bold;
+
+border: 1px solid #cccccc;
+-moz-border-radius: 7px;
+-webkit-border-radius: 7px;
+border-radius: 7px;
+
+-moz-box-shadow: 2px 2px 2px #cccccc;
+-webkit-box-shadow: 2px 2px 2px #cccccc;
+box-shadow: 2px 2px 2px #cccccc;
+}
   .rank {
     font-size: 1.4em;
     color: #0faf87;
